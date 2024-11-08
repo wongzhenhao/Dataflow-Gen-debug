f6ac4ceff0d2983c8b6a09e412f70b56dd3ad3df
Ningxin Pan <98455164+pnx2003@users.noreply.github.com>
2024-11-08 21:07:10 +0800

    Update generate_video.py

--- generate_video.py
+++ generate_video.py
@@ -9,20 +9,6 @@ from src.config import init_generation_config
 from src.core.GeneratorWrapper import GeneratorWrapper
 def main():
     cfg = init_generation_config()
-    
-    # captioner = CaptionerWarpper(
-    #                 meta_image_path=cfg.meta_image_path, 
-    #                 image_folder=cfg.image_folder, 
-    #                 save_folder=cfg.save_folder, 
-    #                 save_per_batch=cfg.save_per_batch,
-    #             )
-    # for captioner_name, model_config in cfg.captioners.items():
-    #     captioner.generate_for_one_model(
-    #                         model_name=captioner_name, 
-    #                         repeat_time=model_config['repeat_time'], 
-    #                         batch_size=model_config['batch_size'],
-    #                         model_config=model_config)
-    # create a generator wrapper
     generator = GeneratorWrapper(
                     meta_prompt_path=cfg.meta_prompt_path, 
                     save_folder=cfg.save_folder, 
